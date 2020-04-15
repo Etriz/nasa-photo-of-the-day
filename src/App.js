0e2bc2899ba95916a7d2f43ea11cd7fb367fcad5
ryanp <ryanpanimation@gmail.com>
2020-04-15 02:08:38 -0500

    add variable date state

--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ import TEST from "./TEST-DATA.json";
 function App() {
   const [data, setData] = useState({});
   const [test] = useState(TEST);
-  // useEffect(() => {
-  //   axios
-  //     .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setData(res.data);
-  //     })
-  //     .catch((err) => console.error("Error!", err));
-  // }, []);
+  const [date, setDate] = useState("");
+
+  const photoDate = () => {
+    let todayDate = new Date(Date.now());
+    let month = todayDate.getMonth() + 1;
+    let day = todayDate.getDate();
+    let year = todayDate.getFullYear();
+    setDate(`${year}-${month}-${day}`);
+  };
+  useEffect(() => {
+    photoDate();
+    // axios
+    //   .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+    //   .then((res) => {
+    //     console.log(res.data);
+    //     setData(res.data);
+    //   })
+    //   .catch((err) => console.error("Error!", err));
+  }, []);
   return (
     <div className="App">
       <Header data={test} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default function Header(props) {
-  const photoDate = () => {
-    const str = props.data.date;
-    const dateStr = str.split("-");
-    const [year, month, day] = dateStr;
-    let today = new Date(year, month - 1, day);
-    return today.toDateString();
-  };
+  // const photoDate = () => {
+  //   const str = props.data.date;
+  //   const dateStr = str.split("-");
+  //   const [year, month, day] = dateStr;
+  //   let today = new Date(year, month - 1, day);
+  //   return today.toDateString();
+  // };
   return (
     <header>
       <h1>NASA's Daily Photo</h1>
-      <p>{photoDate()}</p>
+      <p>{props.data.date}</p>
     </header>
   );
 }
