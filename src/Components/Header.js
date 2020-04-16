b63b8fbede7bd42f25f3eefacf88f2ddb552433a
ryanp <ryanpanimation@gmail.com>
2020-04-16 01:42:19 -0500

    calendar date picker styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Calendar from "react-date-picker";
+// import "react-calendar/dist/Calendar.css";
 
 export default function Header(props) {
-  // const photoDate = () => {
-  //   const str = props.data.date;
-  //   const dateStr = str.split("-");
-  //   const [year, month, day] = dateStr;
-  //   let today = new Date(year, month - 1, day);
-  //   return today.toDateString();
-  // };
+  const dateConversion = () => {
+    const str = props.data.date;
+    const dateStr = str.split("-");
+    const [year, month, day] = dateStr;
+    let today = new Date(year, month - 1, day);
+    return today.toDateString();
+  };
   return (
     <header>
       <h1>NASA's Daily Photo</h1>
-      <p>{props.data.date}</p>
+      {/* <p>{dateConversion()}</p> */}
+      <Calendar selected={new Date(props.data.date)} maxDate={new Date()} value={new Date()} />
     </header>
   );
 }
